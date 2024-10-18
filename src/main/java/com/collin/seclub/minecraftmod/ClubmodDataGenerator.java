ff5aee974d9c35a228bab59eb757e04c6ade72c0
Juanchivp12 <Juanvillegasse@gmail.com>
2024-10-18 12:13:23 -0500

    h

---
+++ src/main/java/com/collin/seclub/minecraftmod/ClubmodDataGenerator.java
@@ -0,0 +1,11 @@
+package com.collin.seclub.minecraftmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ClubmodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
